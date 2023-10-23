0f91a71713cb852ccd8ce12416fbf0b2503b7e62
a.demirov <apdemirov@gmail.com>
2023-10-23 21:24:45 +0300

    fix return to buying

--- booking_app/app/services/site_service.rb
+++ booking_app/app/services/site_service.rb
@@ -4,11 +4,8 @@ require 'json'
 # сообщает pricing об отмене брони
 class SiteService
   def self.call(date:, category:)
-    p date
     uri = URI.parse("http://pricing_app:3003/events/#{date}/#{category}")
     request = Net::HTTP::Patch.new(uri)
     response = Net::HTTP.start(uri.hostname, uri.port).request(request)
-    p response.code
-    p response.body
   end
 end
